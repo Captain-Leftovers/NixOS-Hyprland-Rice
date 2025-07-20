0c380ad6697dccd523f8787fe3d5dd5258e27292
BeeOnDWeb <beeondweb@gmail.com>
2025-07-20 12:58:06 +0200

    added bluetooth module

--- modules/core/bluetooth.nix
+++ modules/core/bluetooth.nix
@@ -5,6 +5,9 @@
 
   services.blueman.enable = true;
 
-  # Optional: allow user access
- # users.users.${config.networking.hostName}.extraGroups = [ "bluetooth" ];
+  environment.systemPackages = with pkgs; [
+    blueman
+  ];
+
+  users.users.beeondweb.extraGroups = [ "bluetooth" ];
 }
