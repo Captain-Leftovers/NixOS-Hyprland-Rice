143823458673b5c2cf32f6cbeb2b6613a369eb67
beeondweb <beeondweb@gmail.com>
2025-07-30 07:41:13 +1000

    added udev.nix rule to change permission for writing keyboard backlight

--- modules/core/udev.nix
+++ modules/core/udev.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  environment.etc."tmpfiles.d/kbd-backlight.conf".text = ''
-    f /sys/class/leds/tpacpi::kbd_backlight/brightness 0666 root root -
-  '';
+   environment.etc."tmpfiles.d/kbd-backlight.conf".text = ''
+     f /sys/class/leds/tpacpi::kbd_backlight/brightness 0666 root root -
+   '';
 }
