a8d14fe076cffc5a9e22ce899c1d885f047230f9
beeondweb <beeondweb@gmail.com>
2025-08-03 08:34:32 +1000

    added nvim now will test if plugins are installed

--- modules/home/neovim-nvf/config.nix
+++ modules/home/neovim-nvf/config.nix
@@ -22,15 +22,12 @@
           name = "gruvbox";
           style = "dark";
         };
+        
         lsp.enable = true;
         treesitter.enable = true;
-
-        # Optional but nice
         statusline.lualine.enable = true;
-
-        # cmp.enable = true; # Completion engine
-        # autopairs.enable = true; # Automatically close brackets
         telescope.enable = true; # Fuzzy finder
+        
       };
     };
   };
