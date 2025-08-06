be5391562aea322154669f3cf37bf944dfac7e1d
beeondweb <beeondweb@gmail.com>
2025-08-06 12:56:47 +1000

    removed nvf nvim and prepare to install kickstart nvim with nighty hopefully

--- modules/home/neovim-nvf/config.nix
+++ modules/home/neovim-nvf/config.nix
@@ -6,67 +6,9 @@
 }:
 
 {
-  imports = [ inputs.nvf.homeManagerModules.default ];
 
-        
-
-  programs.nvf = {
-    enable = true;
-
-    settings = {
-     vim = {
  
-      package =
-        inputs.neovim-overlay.packages.${pkgs.system}.neovim;
-
-      theme = {
-        enable = true;
-        name = "gruvbox";
-        style = "dark";
-      };
-
-        lsp = {
-  enable = true;
+     # package = inputs.neovim-overlay.packages.${pkgs.system}.neovim;
         
-  servers = {
-    nixd = {
-        
-      enable = true;
-         lspconfig = {
-                filetypes = [ "nix" ];
-              };
-    };
 
-    # Do not include nil_ls at all
-     # ❌ REMOVE or DISABLE: nil_ls
-            nil_ls = {
-              enable = false;
-            };
-  };
-};
-
-      treesitter = {
-      enable = true;
-
-      };
-
-      statusline.lualine.enable = true;
-      telescope = {
-      enable = true;
-        
-        };
-      #Languages support
-      languages = {
-      #  nix.enable = true; #not usre if this overrides nixd wth nil_ls if enabled
-        ts.enable = true;
-        };
-
-
-      # Updated namespaces
-      autocomplete."nvim-cmp".enable = true;
-      autopairs."nvim-autopairs".enable = true;
-      mini."ai".enable = true; # for in-editor LLM UI
-      };
-    };
-  };
 }
