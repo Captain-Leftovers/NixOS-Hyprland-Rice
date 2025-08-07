ff2c8700a9663869f43a762e2486cb19375ea578
beeondweb <beeondweb@gmail.com>
2025-08-07 21:49:00 +1000

    added gammastep

--- modules/home/neovim/config.nix
+++ modules/home/neovim/config.nix
@@ -8,13 +8,13 @@
 {
   
  
-     # package = inputs.neovim-overlay.packages.${pkgs.system}.neovim;
+ #  package = inputs.neovim-overlay.packages.${pkgs.system}.neovim;
         
    programs.neovim = {
     enable = true;
 
     # ✅ Use nightly Neovim from your overlay
-    package = pkgs.neovim;
+  #  package = pkgs.neovim;
 
     # Optional: set alias so `vim` launches `nvim`
     viAlias = true;
